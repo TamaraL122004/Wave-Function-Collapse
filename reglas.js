bd7bb8885129ee3fa27e9196b4217f00a6faf318
TamaraL122004 <tamara.lamas@ucuenca.edu.ec>
2025-01-05 20:48:45 -0500

    Act Final

--- reglas.js
+++ reglas.js
@@ -2,11 +2,11 @@ const azulejos = [];
 const reglas = [
   { UP: 0, RIGHT: 2, DOWN: 4, LEFT: 0 }, //0
   { UP: 0, RIGHT: 0, DOWN: 5, LEFT: 2 }, //1
-  { UP: 0, RIGHT: 2, DOWN: 6, LEFT: 2 }, //2
+  { UP: 0, RIGHT: 2, DOWN: 6, LEFT: 2 }, //2//
   { UP: 0, RIGHT: 0, DOWN: 3, LEFT: 2 }, //3
   { UP: 4, RIGHT: 6, DOWN: 4, LEFT: 0 }, //4
   { UP: 5, RIGHT: 0, DOWN: 5, LEFT: 6 }, //5
-  { UP: 6, RIGHT: 3, DOWN: 0, LEFT: 3 }, //6
+  { UP: 6, RIGHT: 3, DOWN: 0, LEFT: 3 }, //6//
   { UP: 3, RIGHT: 0, DOWN: 0, LEFT: 3 }, //7
   { UP: 4, RIGHT: 4, DOWN: 0, LEFT: 0 }, //8
   { UP: 5, RIGHT: 0, DOWN: 0, LEFT: 4 }, //9
